d5ada042e83363f0fd55bdd5a1aefaf9c5480625
ЗахаровСД <@101-08>
2022-09-07 18:10:48 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Contr
+    {
+        public double zd1()
+        {
+            double a, b;
+            Console.WriteLine("Введите число");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите число");
+            b = Convert.ToDouble(Console.ReadLine());
+            return Math.Log(a, b);
+        }
+        public bool zd3()
+        {
+            double a;
+            Console.WriteLine("Введите число");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Остаток при делении на 3 " + a % 3 + " остаток при делении на 4 " + a % 4);
+            return (a % 3 == a % 4);
+        }
+        public void zd2()
+        {
+            double a, b, c, d;
+            Console.WriteLine("Введите число");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите число");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите число");
+            c = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите число");
+            d = Convert.ToDouble(Console.ReadLine());
+            double per = Math.Abs(b - a) + Math.Abs(d - c)*2;
+            double sq = Math.Abs(b - a) * Math.Abs(d - c);
+            Console.WriteLine("Sq = " + sq + "Per = " + per);
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using ConsoleApp1;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Contr contr = new Contr();
+            while (true)
+            {
+                Console.WriteLine("Введите номер задачи");
+                int otw = Convert.ToInt32(Console.ReadLine());
+
+                switch (otw)
+                {
+                    case 1:
+                        Console.WriteLine("Ответ: " + contr.zd1());
+                        break;
+                    case 2:
+                        contr.zd2();
+                        break;
+                    case 3:
+                        Console.WriteLine("Ответ: " + contr.zd3());
+                        break;
+                }
+            }
+
+        }
+    }
+}
